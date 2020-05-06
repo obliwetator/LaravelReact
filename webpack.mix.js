90c728e6cce85b7a961d7464acfddabe84e18185
Patryk <patrykstyla22@gmail.com>
2020-05-06 15:45:36 +0100

    misc

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.sass('resources/sass/app.scss', 'public/css');
+// mix.sass('resources/sass/app.scss', 'public/css');
 
 // mix.react('resources/js/app.js', 'public/js').sourceMaps()
 
